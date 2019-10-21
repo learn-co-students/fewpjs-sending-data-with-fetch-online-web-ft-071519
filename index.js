a7dd2740395545bac512eec69300f85dc4090da3
Audrea Cook <audreacwork@gmail.com>
2019-10-21 15:29:24 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    let configObj = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({name: userName, email: userEmail})
+    };
+
+    return fetch('http://localhost:3000/users', configObj)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (object) {
+            console.log(object)
+            document.body.innerHTML = object["id"];
+
+        })
+        .catch(function (error) {
+            alert("Houston, we've got a problem");
+            console.log(error.message);
+            document.body.innerHTML = error.message
+        })
+}
