8293eb0556878327719b998709147deb5ba68ed1
radville <laura.radville@gmail.com>
2019-10-22 12:40:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    let formData = {
+        name: userName,
+        email: userEmail
+        };
+        
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+        };
+    
+    return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(object) {
+            let id = object.id;
+            let div = document.createElement("div")
+            div.innerText = id
+            document.getElementsByTagName("body")[0].appendChild(div)
+        })
+        .catch(function(error) {
+            if (error.message) {
+                let errorNotice = document.createElement("div")
+                errorNotice.innerText = error.message
+                document.getElementsByTagName("body")[0].appendChild(errorNotice)
+            }
+        })
+}
